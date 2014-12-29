782a64072049a0d2238974447269f8aa7c79ec36
Daniel <BurtDaniel@Gmail.com>
2014-12-28 23:42:40 -0800

    finished danny-search

--- danny-search.rb
+++ danny-search.rb
@@ -1,48 +1,54 @@
 class Node
-    attr_accessor :char, :locations
-    def initialize(char = nil)
-        self.char       = char
-        self.locations  = Hash.new { Array.new }
-    end
+  attr_accessor :char, :locations
+  def initialize(char = nil)
+    self.char       = char
+    self.locations  = Hash.new { |h, k| h[k] = Array.new }
+  end
   
-    def occurances
-        self.locations.keys
-    end
+  def occurances
+    self.locations.keys
+  end
   
-    def nodes_at(location)
-        self.locations(location)
-    end
+  def [](pos)
+    self.locations[pos]
+  end
   
-    def insert(location, leftNode, rightNode)
-        self.locations[location] = [leftNode, rightNode]
-    end
+  def insert(location, leftNode, rightNode)
+    self.locations[location] = [leftNode, rightNode]
+  end
 end
 
 
 
 class Search
-    attr_accessor :my_words, :chars
-    def initialize(str)
-        self.my_words   = str
-        self.chars      = Hash.new { |hash, key| hash[key] = Node.new(key) }
-    end
+  attr_accessor :my_words, :chars
+  def initialize(str)
+    self.my_words   = str
+    self.chars      = Hash.new { |h, k| h[k] = Node.new(k) }
+  end
     
-    def buildMap
-        (0...self.my_words.length).each do |i|
-            c, l, r = self.my_words[i], self.my_words[i - 1], self.my_words[i + 1]
-            self.chars[c].insert(i, self.chars[l], self.chars[r])
-        end
+  def buildMap
+    (0...self.my_words.length).each do |i|
+      c, l, r = self.my_words[i], self.my_words[i - 1], self.my_words[i + 1]
+      self.chars[c].insert(i, self.chars[l], self.chars[r])
     end
+  end
     
-    def searchNode(node, location, str)
-    end
+  def searchNode(node, location, str)
+    return nil          if str.empty?
+    return nil          if node[location].last.char != str[1]
+    return location + 1 if str.length == 2
+    searchNode(node[location].last, location + 1, str[1..-1])
+  end
     
-    def search(str)
-        node        = self.chars[str[0]]
-        locations   = node.occurances
-    end
+  def search(str)
+    node  = self.chars[str[0]]
+    return node.occurances if str.length == 1
+    (node.occurances.map { |l| searchNode(node, l, str) } - [nil]).map { |x| x - str.length + 1 }
+  end
 end
 
-s = Search.new("Hello world, I am some text that is going to be used for searching")
-s.buildMap
+# s = Search.new("abc abc def abc")
+# s.buildMap
+# puts s.search(" ").join(", ")
     
