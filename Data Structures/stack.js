7817777d10fc35497eed6e91aec93f57f02aa17e
Isis <isisAnchalee@users.noreply.github.com>
2014-12-30 12:15:26 -0800

    Update stack.js

--- Data Structures/stack.js
+++ Data Structures/stack.js
@@ -22,32 +22,32 @@ Stack.prototype.pop = function() {
 
 // Using Array
 
-	function Stack(){
-		this.dataStore = [];
-		this.top = 0;
-		this.push = push;
-		this.pop = pop;
-		this.peek = peek;
-		this.length = length;
-		this.clear = clear;
-	};
+function Stack(){
+	this.dataStore = [];
+	this.top = 0;
+	this.push = push;
+	this.pop = pop;
+	this.peek = peek;
+	this.length = length;
+	this.clear = clear;
+};
 
-	function push(val){
-		this.dataStore[this.top++] = val;
-	};
+function push(val){
+	this.dataStore[this.top++] = val;
+};
 
-	function pop(){
-		return this.dataStore[--this.top];
-	};
+function pop(){
+	return this.dataStore[--this.top];
+};
 
-	function peek(){
-		return this.dataStore[this.top-1];
-	};
+function peek(){
+	return this.dataStore[this.top-1];
+};
 
-	function length(){
-		return this.top;
-	};
+function length(){
+	return this.top;
+};
 
-	function clear(){
-		this.top = 0;
-	};
+function clear(){
+	this.top = 0;
+};
