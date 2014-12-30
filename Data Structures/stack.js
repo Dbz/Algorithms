4f6061c46f17e8b01231a5c44a8fa37446072f87
Isis <isisAnchalee@users.noreply.github.com>
2014-12-30 12:14:37 -0800

    add array stack

--- Data Structures/stack.js
+++ Data Structures/stack.js
@@ -19,3 +19,35 @@ Stack.prototype.pop = function() {
   this.head = this.head.nextNode;
   return node.value;
 }
+
+// Using Array
+
+	function Stack(){
+		this.dataStore = [];
+		this.top = 0;
+		this.push = push;
+		this.pop = pop;
+		this.peek = peek;
+		this.length = length;
+		this.clear = clear;
+	};
+
+	function push(val){
+		this.dataStore[this.top++] = val;
+	};
+
+	function pop(){
+		return this.dataStore[--this.top];
+	};
+
+	function peek(){
+		return this.dataStore[this.top-1];
+	};
+
+	function length(){
+		return this.top;
+	};
+
+	function clear(){
+		this.top = 0;
+	};
