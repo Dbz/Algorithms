d1a524f777d78f5b309291d3e4aac718fad0a2a1
Isis Anchalee <isiswenger@gmail.com>
2014-12-30 12:51:34 -0800

    Adding balanced parens

---
+++ Algorithms/balancedparens.js
@@ -0,0 +1,20 @@
+//Balanced Parens: Write a function that takes an arithmetic expression as an argument and returns true or false for an expression where a parenthesis is missing. 
+
+function balancedParens(input){
+	var stack = new Stack();
+
+	for (var i = 0; i < input.length; i++) {
+	    var c = input[i];
+	    if (c === '(' || c === '{' || c === '[') {
+	      stack.push(c);
+	    } else if (c === ')' || c === '}' || c === ']') {
+	      var last = stack.pop();
+	      if (!last || (c === ')' && last !== '(') || (c === '}' && last !== '{') || (c === ']' && last !== '[')) {
+	        return false;
+	      }
+	    }
+	  }
+
+return (stack.length() === 0);
+
+}
