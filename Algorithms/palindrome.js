ee7c92282517f9a9c72f6c71ebb128225512ccc3
Isis Anchalee <isiswenger@gmail.com>
2014-12-30 13:06:38 -0800

    fixing indentation

--- Algorithms/palindrome.js
+++ Algorithms/palindrome.js
@@ -1,16 +1,16 @@
 //Using a stack class, determine if a string is a palindrome.
 
 function Palindrome(str){
-	var stack = new Stack(),
-		  rWord = "";
+  var stack = new Stack(),
+      rWord = "";
 
-	for (var i = 0; i < str.length; i++){
-		stack.push(str[i]);
+  for (var i = 0; i < str.length; i++){
+    stack.push(str[i]);
+  }
+
+  while (stack.length() !== 0){
+    rWord += stack.pop();
 	}
 
-	while (stack.length() !== 0){
-		rWord += stack.pop();
-	}
-
-	return str === rWord;
+  return str === rWord;
 }
