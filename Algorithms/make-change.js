f4e5a6ebd86940ddaa797b111fdcda14551bac44
isisanchalee <isis.wenger@onelogin.com>
2015-04-13 13:40:49 -0700

    Add iterative make change algorithm

---
+++ Algorithms/make-change.js
@@ -0,0 +1,20 @@
+// Write a function that given an amount of money and a list of coin denominations computes the number of ways to make amount of money with coins of the available denominations.
+
+function makeChange(amount, coins){
+
+	var waysOfDoingNCents = []
+	for (var i = 0; i < amount + 1; i++){
+		waysOfDoingNCents[i] = 0;
+	}
+
+	waysOfDoingNCents[0] = 1;
+
+	for (var coin = 0; coin < coins.length; coin++){
+		for (var higherAmt = coin; higherAmt < amount + 1; higherAmt++){
+			higherAmtRemainder = higherAmt - coin;
+			waysOfDoingNCents[higherAmt] += waysOfDoingNCents[higherAmtRemainder] 
+		}
+	}
+
+	return waysOfDoingNCents[amount];
+}
