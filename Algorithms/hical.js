32998a735c8be01544b67b3b917a68acce20329b
Daniel <BurtDaniel@Gmail.com>
2015-09-17 23:22:23 -0700

    added remaining links to readme; minor file changes

--- Algorithms/hical.js
+++ Algorithms/hical.js
@@ -1,4 +1,10 @@
-//Your company built an in-house calendar tool called HiCal. You want to add a feature to see the times in a day when everyone is available. To do this, you’ll need to know when any team is having a meeting. In HiCal, a meeting is stored as a tuple of integers [start_time, end_time] . These integers represent the number of 30-minute blocks past 9:00am. Writ a function that takes an array of meeting times and returns a condensed version.
+// Your company built an in-house calendar tool called HiCal.
+// You want to add a feature to see the times in a day when everyone is available.
+// To do this, you’ll need to know when any team is having a meeting.
+// In HiCal, a meeting is stored as a tuple of integers [start_time, end_time].
+// These integers represent the number of 30-minute blocks past 9:00am.
+// Writ a function that takes an array of meeting times and returns a condensed version.
+
 times = [[0, 1], [3, 5], [4, 8], [10, 12], [9, 10]]
 //  [(0, 1), (3, 8), (9, 12)]
 function hiCal(times){
