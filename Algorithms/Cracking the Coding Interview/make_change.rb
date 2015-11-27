4ccb660b5f3056a2e29dac95f5278eca66db7f53
isisanchalee <isis.wenger@onelogin.com>
2015-11-26 18:31:24 -0800

    Fix make change ruby

--- Algorithms/Cracking the Coding Interview/make_change.rb
+++ Algorithms/Cracking the Coding Interview/make_change.rb
@@ -1,3 +1,13 @@
 def make_change(amount, coins)
+ result = Array.new(amount + 1, 0)
+ result[0] = 1
 
+ coins.each do |coin|
+  (coin..amount).each do |higher_amount|
+    remainder = higher_amount - coin
+    result[higher_amount] += result[remainder]
+  end
+ end
+
+ result[amount]
 end
