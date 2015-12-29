79b772b057073d036828e16ed7f1465bf6f9e89c
isisanchalee <isis.wenger@onelogin.com>
2015-12-29 11:51:52 -0800

    Fix BST thing

--- Algorithms/Ruby/two-sum.rb
+++ Algorithms/Ruby/two-sum.rb
@@ -1,4 +1,7 @@
 # Given an integer x and a sorted array a of N distinct integers, design a linear-time algorithm to determine if there exists two distinct indices i and j such that arr[i] + arr[j] == x
+# @param {Array[]} arr
+# @return {Boolean}
+
 def two_sum(arr, x)
   seen = {}
 
