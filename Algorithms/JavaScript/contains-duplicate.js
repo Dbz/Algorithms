843472102f54ad6559255d2e741eaff4ba95f01d
Daniel <BurtDaniel@Gmail.com>
2016-01-02 10:36:26 +0700

    Refactored contains duplicate

--- Algorithms/JavaScript/contains-duplicate.js
+++ Algorithms/JavaScript/contains-duplicate.js
@@ -1,16 +1,14 @@
 /**
- * Given an array of integers, find if the array contains any duplicates. Your function should return true if any value appears at least twice in the array, and it should return false if every element is distinct.
- * @param {number[]} nums
+ * Given an array of integers, find if the array contains any duplicates. Your function should return true if any
+ * value appears at least twice in the array, and it should return false if every element is distinct.
+ * @param {number[]} numbers
  * @return {boolean}
  */
-var containsDuplicate = function(nums) {
-  var hashMap = {};
-  for (var i = 0; i < nums.length; i++) {
-    if (hashMap[nums[i]]) {
-        return true;
-    } else {
-        hashMap[nums[i]] = 1;
-    }
+var containsDuplicate = function(numbers) {
+  var visitedNumbers = {};
+  for (var i = 0; i < numbers.length; i++) {
+    if (visitedNumbers[numbers[i]]) return true;
+    visitedNumbers[numbers[i]] = 1;
   }
   
   return false;
