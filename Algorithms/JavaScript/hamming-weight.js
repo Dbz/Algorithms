1a4f48a354e9057ea23f5b8d4a45dcdf14172e90
Daniel <BurtDaniel@Gmail.com>
2016-01-02 10:37:36 +0700

    Renamed hamming weight number variable

--- Algorithms/JavaScript/hamming-weight.js
+++ Algorithms/JavaScript/hamming-weight.js
@@ -1,8 +1,8 @@
 /**
  * Write a function that takes an unsigned integer and returns the number of ’1' bits it has (also known as the Hamming weight).
- * @param {number} n - a positive integer
+ * @param {number} number - a positive integer
  * @return {number}
  */
-var hammingWeight = function(n) {
-  return n.toString(2).split("1").length - 1
+var hammingWeight = function(number) {
+  return number.toString(2).split("1").length - 1
 };
