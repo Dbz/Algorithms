fd8a4f016636b96f9f94c81611002bc07832d84c
Daniel <BurtDaniel@Gmail.com>
2016-01-05 16:27:32 +0700

    Renamed variables in subsets

--- Algorithms/JavaScript/subsets.js
+++ Algorithms/JavaScript/subsets.js
@@ -1,16 +1,14 @@
 // Write a function that will output all subsets of an array.
 
 function subsets(array) {
-  if (array.length === 0) {
-    return [[]];
-  }
+  if (array.length === 0) return [[]];
 
-  var firstElement = array[0];
-  var subSubsets = subsets(array.slice(1));
+  var firstElement   = array[0];
+  var partialSubsets = subsets(array.slice(1));
 
-  var newSubsets = subSubsets.map(function(subSubset) {
-    return [firstElement].concat(subSubset);
+  var longestSubsets = partialSubsets.map(function(subset) {
+    return [firstElement].concat(subset);
   });
 
-  return subSubsets.concat(newSubsets);
+  return longestSubsets.concat(partialSubsets);
 }
