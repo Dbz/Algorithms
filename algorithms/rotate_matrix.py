1ac65c34ae2a0c45b0330ea7c95f22dfeabb7549
NimbusScale <joe@nimbusscale.com>
2017-08-01 10:11:58 -0700

    doctest spacing to help readability

--- algorithms/rotate_matrix.py
+++ algorithms/rotate_matrix.py
@@ -6,23 +6,19 @@ Given an image represented by an NxN matrix, where each pixel in the image is
 place?
 
 Odd sized matrix
->>> rotate_matrix([['q','w','e'],['a','s','d'],['z','x','c']])
+>>> rotate_matrix([['q', 'w', 'e'], ['a', 's', 'd'], ['z', 'x', 'c']])
 [['z', 'a', 'q'], ['x', 's', 'w'], ['c', 'd', 'e']]
 
 Even sized matrix
->>> rotate_matrix([['1','2','3','4'],['q','w','e','r'],['a','s','d','f'],['z','x','c','v']])
+>>> rotate_matrix([['1', '2', '3', '4'], ['q', 'w', 'e', 'r'], ['a', 's', 'd', 'f'], ['z', 'x', 'c', 'v']])
 [['z', 'a', 'q', '1'], ['x', 's', 'w', '2'], ['c', 'd', 'e', '3'], ['v', 'f', 'r', '4']]
 
 Solution looks at each element in each list making it O(E*L)
 """
+
 
 def rotate_matrix(matrix):
     """combine the elements of each list with zip and then iterate through the
     lists in reverse using a slice with a step of -1.
     """
     return list(list(x)[::-1] for x in zip(*matrix))
-
-
-
-
-
