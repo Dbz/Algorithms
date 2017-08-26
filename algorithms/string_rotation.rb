86dffad94cac3b1e5518375015f49ca2666811e3
daniel.burt <daniel.burt@workday.com>
2017-08-26 11:28:18 +0100

    added problem number

--- algorithms/string_rotation.rb
+++ algorithms/string_rotation.rb
@@ -1,3 +1,4 @@
+# CtCI 6th Edition Problem 1.9
 # String Rotation: Assume you have a method isSubst ring which checks if one word is a substring
 # of another. Given two strings, 51 and 52, write code to check if 52 is a rotation of 51 using only one
 # call to isSubstring (e.g., "waterbottle" is a rotation of"erbottlewat").
