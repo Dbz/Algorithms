b2dea791ccdc21ff1bed869a68be1cfcf4e5e7d0
Danny Burt <Dbz@users.noreply.github.com>
2018-09-18 13:24:41 -0700

    Fixed random characters

--- algorithms/loop_detection.rb
+++ algorithms/loop_detection.rb
@@ -5,7 +5,7 @@
 # Circular linked list: A (corrupt) linked list in which a node's next pointer points to an earlier node, so
 # as to make a loop in the linked list.
 # EXAMPLE
-# Input: A -) B -) C -) 0 -) E - ) C[thesameCasearlierl
+# Input: A -) B -) C -) D -) E -) C
 # Output: C
 
 require 'Set'
